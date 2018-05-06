0615593fe8b1208d1cb8235a46044f420abc822a
Brian <brian.macmonigle@gmail.com>
2018-05-06 19:45:49 -0400

    Chapter 9 Finish

--- src/game/DummyGame.java
+++ src/game/DummyGame.java
@@ -39,8 +39,8 @@ public class DummyGame implements IGameLogic {
         renderer.init(window);
     
         
-        // Mesh mesh = OBJLoader.loadMesh("/models/bunny.obj");
-        Mesh mesh = OBJLoader.loadMesh("/models/cube.obj");
+        //Mesh mesh = OBJLoader.loadMesh("/models/bunny.obj");
+         Mesh mesh = OBJLoader.loadMesh("/models/cube.obj");
         Texture texture = new Texture("/textures/grassblock.png");
         mesh.setTexture(texture);
         GameItem gameItem = new GameItem(mesh);
@@ -67,11 +67,6 @@ public class DummyGame implements IGameLogic {
             cameraInc.y = -1;
         } else if(window.isKeyPressed(GLFW_KEY_X)) {
             cameraInc.y = 1;
-        }
-        if(window.isKeyPressed(GLFW_KEY_TAB)) {
-            glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-        } else {
-            glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
         }
     }
     
